7101702de17054275df8c4898a566c8063487374
Eric Petersen <erictheredbaron@gmail.com>
2019-08-29 18:34:45 -0700

    first commit

---
+++ copy_organize_photos.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Bash script copies photos from input directory to
+# 	output directory and organizes them by date
+#	in the output directory.
+#	E.G. Photo on Dec 23 2013 goes to:
+#	$outputdir/2013/12_December/23_Dec_2013/
+#
+# Usage: ./copy_organize_photos.sh {input directory} {output directory}
+
+in_dir="$1"
+out_dir="$2"
+
+cd "$in_dir"
+# Loop through files
+for file in *
+	do
+	echo "$file"
+	if [ -f "$file" ] # Test if it's a file
+	then 
+		date=$(date -r "$file" +%Y/%m_%B/%d) && # Extract date in directory format
+		dpath="$out_dir/$date/"
+		echo "Copying $file to $dpath"
+		# Create path if it doesn't already exist:
+		if [ -d "$dpath" ]
+			then
+ 				echo "Path exists"
+			else 
+				echo "Making path"
+				mkdir -p "$dpath"
+	
+		fi
+		# Move file to that path:
+		cp -- "$file" "$dpath"
+	fi
+done
